2ed71767d8992428b5e4df825014e2306ba3f481
RuRuTiaSaMa <1009087450@qq.com>
2022-05-23 00:41:26 +0800

    sm8250-common: switch to my own clang
    Signed-off-by: RuRuTiaSaMa <1009087450@qq.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -109,8 +109,12 @@ TARGET_KERNEL_SOURCE := kernel/xiaomi/sm8250
 
 # Kernel Clang
 TARGET_KERNEL_CLANG_COMPILE := true
-TARGET_KERNEL_CLANG_VERSION := sdclang
+TARGET_KERNEL_CLANG_VERSION := maou-clang
 KERNEL_LD := LD=ld.lld
+
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/maou-clang
+TARGET_KERNEL_CROSS_COMPILE_PREFIX := $(shell pwd)/prebuilts/clang/host/linux-x86/maou-clang/bin/aarch64-linux-gnu-
+TARGET_KERNEL_CROSS_COMPILE_ARM32_PREFIX := $(shell pwd)/prebuilts/clang/host/linux-x86/maou-clang/bin/arm-linux-gnueabi-
 TARGET_KERNEL_ADDITIONAL_FLAGS := LD=ld.lld AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip
 TARGET_KERNEL_ADDITIONAL_FLAGS += LLVM=1 LLVM_IAS=1
 TARGET_KERNEL_ADDITIONAL_FLAGS += HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
